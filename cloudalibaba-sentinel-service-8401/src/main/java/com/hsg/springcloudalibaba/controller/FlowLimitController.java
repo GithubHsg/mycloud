71517989e0867190e8e2205f495b6ff3981f0c58
Hsg <694102512@qq.com>
2021-02-26 19:21:32 +0800

    113_Sentinel监控项目

---
+++ cloudalibaba-sentinel-service-8401/src/main/java/com/hsg/springcloudalibaba/controller/FlowLimitController.java
@@ -0,0 +1,24 @@
+package com.hsg.springcloudalibaba.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+@RequestMapping("/flowLimit")
+public class FlowLimitController {
+
+    @GetMapping("/testA")
+    public String testA() {
+        return "------testA";
+    }
+
+    @GetMapping("/testB")
+    public String testB() {
+        log.info(Thread.currentThread().getName() + "\t" + "...testB");
+        return "------testB";
+    }
+
+}
