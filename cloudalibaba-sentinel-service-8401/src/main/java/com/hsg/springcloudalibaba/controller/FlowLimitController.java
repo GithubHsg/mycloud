fe9b47d3f814e4d6ebad7bc9f721cb350abc2b75
Hsg <694102512@qq.com>
2021-02-27 13:55:58 +0800

    125_Sentinel热点key

--- cloudalibaba-sentinel-service-8401/src/main/java/com/hsg/springcloudalibaba/controller/FlowLimitController.java
+++ cloudalibaba-sentinel-service-8401/src/main/java/com/hsg/springcloudalibaba/controller/FlowLimitController.java
@@ -1,8 +1,11 @@
 package com.hsg.springcloudalibaba.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -19,6 +22,32 @@ public class FlowLimitController {
     public String testB() {
         log.info(Thread.currentThread().getName() + "\t" + "...testB");
         return "------testB";
+    }
+
+    /**
+     * 热点数据限流
+     * 如果不指定blockHandler，则触发限流时进入默认的错误页面
+     *
+     * @Author:Hsg
+     * @Date: 2021/2/27 下午12:03
+     */
+    @GetMapping("/testHotKey")
+    @SentinelResource(value = "testHotKey", blockHandler = "deal_testHotKey")
+    public String testHotKey(@RequestParam(value = "p1", required = false) String p1,
+                             @RequestParam(value = "p2", required = false) String p2) {
+        //int age = 10/0;
+        return "------testHotKey";
+    }
+
+    /**
+     * 兜底方案
+     * 可自定义页面内容
+     *
+     * @Author:Hsg
+     * @Date: 2021/2/27 下午12:12
+     */
+    public String deal_testHotKey(String p1, String p2, BlockException exception) {
+        return "------deal_testHotKey,o(╥﹏╥)o";  //sentinel系统默认的提示：Blocked by Sentinel (flow limiting)
     }
 
 }
